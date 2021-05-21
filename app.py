d3d0cc9969f237aada4572ef8e627eb3aafb5fc9
Mainak Kundu <mainakmarket@gmail.com>
2021-05-21 14:25:31 +0530

    Updated app

--- app.py
+++ app.py
@@ -7,33 +7,24 @@ import config
 
 app = Flask(__name__)
 
-model = pickle.load(open('regression_model_output_v0.0.1.pkl', 'rb'))
-cols = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare','Embarked']
+#model = pickle.load(open('regression_model_output_v0.0.1.pkl', 'rb'))
+filename = 'finalized_model1.sav'
+loaded_model = pickle.load(open(filename, 'rb'))
+
+cols = ['pclass', 'sex', 'age', 'sibsp', 'parch', 'fare', 'cabin', 'embarked','title']
+
 
 @app.route('/health/',methods=['GET'])
-def heath(text):
+def heath():
     return ('API working!!!')
 
 @app.route('/predict/',methods=['POST'])
 def predict_api_updated():
     event=request.json
-    data_unseen = pd.DataFrame([event])
-    print(data_unseen)
-    data_unseen = data_unseen[cols]
-    prediction = model.predict(data_unseen)
+    data_unseen = pd.DataFrame([event['instances']])
+    data_unseen.columns=cols
+    prediction = str(loaded_model.predict(data_unseen)[0])
     return(jsonify({'predict':prediction}))
-
-
-
-
-@app.route('/predict_api/',methods=['POST'])
-def predict_api():
-    data = request.get_json(force=True)
-    data_unseen = pd.DataFrame([data])
-    data_unseen = data[cols]
-    #prediction = predict_model(model, data=data_unseen)
-    prediction = model.predict(data_unseen)
-    return jsonify(prediction)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=config.PORT, debug=config.DEBUG_MODE)
