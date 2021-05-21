10d3200f05a346ebd04177a1d200821b002b686b
Mainak Kundu <mainakmarket@gmail.com>
2021-05-21 13:30:06 +0530

    pickle it in proper way

--- app.py
+++ app.py
@@ -3,18 +3,30 @@ from flask import Flask,request, url_for, redirect, render_template, jsonify
 import pandas as pd
 import pickle
 import numpy as np
+import config
 
 app = Flask(__name__)
 
 model = pickle.load(open('regression_model_output_v0.0.1.pkl', 'rb'))
 cols = ['Pclass', 'Sex', 'Age', 'SibSp', 'Parch', 'Fare','Embarked']
 
+@app.route('/health/',methods=['GET'])
+def heath(text):
+    return ('API working!!!')
+
+@app.route('/predict/',methods=['POST'])
+def predict_api_updated():
+    event=request.json
+    data_unseen = pd.DataFrame([event])
+    print(data_unseen)
+    data_unseen = data_unseen[cols]
+    prediction = model.predict(data_unseen)
+    return(jsonify({'predict':prediction}))
 
 
 
 
-
-@app.route('/predict_api',methods=['POST'])
+@app.route('/predict_api/',methods=['POST'])
 def predict_api():
     data = request.get_json(force=True)
     data_unseen = pd.DataFrame([data])
